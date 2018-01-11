6fcdbc44acc5d5b8d1a849795d98d5d706389e5d
Auston Stewart <auston@shedbuilt.net>
2018-01-11 01:26:52 -0500

    Preventing python module from building

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sed -i s/3000/5000/ libxslt/transform.c doc/xsltproc.{1,xml}
 ./configure --prefix=/usr \
-            --disable-static || return 1
+            --disable-static \
+            --without-python || return 1
 make -j $SHED_NUMJOBS || return 1
 make DESTDIR="$SHED_FAKEROOT" install || return 1
