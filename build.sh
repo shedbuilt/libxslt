a342a00c06ed55d8d338c00cd300f0b0308d4ba6
Auston Stewart <auston@shedbuilt.net>
2018-04-15 12:19:20 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ sed -i s/3000/5000/ libxslt/transform.c doc/xsltproc.{1,xml}
 ./configure --prefix=/usr \
             --disable-static \
             --without-python || return 1
-make -j $SHED_NUMJOBS || return 1
-make DESTDIR="$SHED_FAKEROOT" install || return 1
+make -j $SHED_NUM_JOBS || return 1
+make DESTDIR="$SHED_FAKE_ROOT" install || return 1
