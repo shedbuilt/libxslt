75c681f610de2b97c39abd192bbfc31f8b756340
Auston Stewart <auston@shedbuilt.net>
2018-10-25 21:04:09 -1000

    Updating build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-sed -i s/3000/5000/ libxslt/transform.c doc/xsltproc.{1,xml}
+# Patch
+sed -i s/3000/5000/ libxslt/transform.c doc/xsltproc.{1,xml} &&
+# Configure
 ./configure --prefix=/usr \
             --disable-static \
-            --without-python || return 1
-make -j $SHED_NUM_JOBS || return 1
-make DESTDIR="$SHED_FAKE_ROOT" install || return 1
+            --without-python &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install
